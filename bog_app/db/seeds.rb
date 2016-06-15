162bea421ec8f3e9ab0b37eff347db01cedc5292
mattlaguardia <matthewlaguardia@gmail.com>
2016-06-15 11:31:25 -0700

    initial commit

---
+++ bog_app/db/seeds.rb
@@ -0,0 +1,6 @@
+Creature.create([
+  {name: "Gollum", description: "originally known as Sméagol, was at first a Stoor, one of the three early Hobbit-types. Deformed and twisted in body and mind by the corruption of the Ring, his chief desire was to possess the very Ring that had enslaved him."},
+  {name: "Swamp Thing", description: "A humanoid mass of vegetable matter who fights to protect his swamp home, the environment in general, and humanity from various supernatural or terrorist threats."},
+  {name: "Yoda", description: "900-year-old Jedi master. Lives in a swamp on Degoba. Steals food and giggles."},
+  {name: "Bernardo-Ben", description: "A four-eyed creature living on bart"}
+])
